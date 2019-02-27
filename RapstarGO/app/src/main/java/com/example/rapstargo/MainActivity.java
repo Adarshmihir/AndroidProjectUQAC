116843626741a0988ca858aa1c67d2ad6ddeb30e
Adarshmihir <franck431@hotmail.fr>
2019-02-27 15:07:38 -0500

    Added branch Signup and added Signup interface to it

--- RapstarGO/app/src/main/java/com/example/rapstargo/MainActivity.java
+++ RapstarGO/app/src/main/java/com/example/rapstargo/MainActivity.java
@@ -11,14 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent hub = new Intent(MainActivity.this, HubActivity.class);
-        Intent main = new Intent(MainActivity.this, MainActivity.class);
-        Intent maps = new Intent(MainActivity.this, MapsActivity.class);
-        Intent signin = new Intent(MainActivity.this, SigninActivity.class);
         Intent signup = new Intent(MainActivity.this, SignupActivity.class);
 
         /* Pour tester */
-        // startActivity(INTENT_NAME);
         startActivity(signup);
 
     }
