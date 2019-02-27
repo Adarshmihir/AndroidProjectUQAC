768f78c4b390bb1a3fec5284c758eb872693fd75
Adarshmihir <franck431@hotmail.fr>
2019-02-27 15:26:56 -0500

    Little arrangement

--- RapstarGO/app/src/main/java/com/example/rapstargo/MainActivity.java
+++ RapstarGO/app/src/main/java/com/example/rapstargo/MainActivity.java
@@ -11,10 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+         Intent main = new Intent(MainActivity.this, MainActivity.class);
 
         /* Pour tester */
-       // Intent myIntent = new Intent(MainActivity.this, MapsActivity.class);
-        // startActivity(myIntent);
+        startActivity(main);
 
     }
 }
