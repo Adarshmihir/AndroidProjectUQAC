2cc5e8ee479e900d317f67cb39dce02fa819128d
Adarshmihir <franck431@hotmail.fr>
2019-03-06 14:47:17 -0500

    Added empty files

---
+++ RapstarGO/app/src/main/java/com/example/rapstargo/CreateLobbyActivity.java
@@ -0,0 +1,6 @@
+package com.example.rapstargo;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateLobbyActivity extends AppCompatActivity {
+}
--- RapstarGO/app/src/main/java/com/example/rapstargo/MainActivity.java
+++ RapstarGO/app/src/main/java/com/example/rapstargo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         Intent lobby = new Intent(MainActivity.this, LobbyActivity.class);
+         Intent createLobby = new Intent(MainActivity.this, CreateLobbyActivity.class);
 
         /* Pour tester */
         startActivity(lobby);
