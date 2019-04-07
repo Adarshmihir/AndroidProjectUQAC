f4f3a1034fc7dfa94a25b028e886d64617f07628
unknown <etudiant@DIM-P46450-03.laboratoire.uqac.ca>
2019-04-07 14:45:10 -0400

    l

--- RapstarGO/app/src/main/java/com/example/rapstargo/SignupActivity.java
+++ RapstarGO/app/src/main/java/com/example/rapstargo/SignupActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 public class SignupActivity extends AppCompatActivity {
     
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
